ae775bbf950c545c6eace3144d911bf68059f312
bikramjit ganguly <gangulybikramjit@hotmail.com>
2012-08-19 18:32:59 -0700

    one more version where the layout is fluid
    redid css/redid js also a little bit, just for optimisation

--- WebContent/scripts/getJarDemo.js
+++ WebContent/scripts/getJarDemo.js
@@ -13,32 +13,33 @@ NS.catgImgArray["Social.jpg"]=7;
 NS.catgNextImgArray=["Education.jpg","Entertainment.jpg","Finance.jpg","Food.jpg","Games.jpg","Lifestyle.jpg","Productivity.jpg","Social.jpg"];
 
 //basically , if the nav arrows are clicked, get a reference to some baseline img src attr, then get the next/prev idx from the assciative array,
-//then lookup the corresponding image src from the NextImgArray
-//could have been improved by using a single json object ?
+//then lookup the corresponding image src from the NextImgArray. could have been improved by using a single json object ?
 
 $(document).ready(function() {
    $('.galleryRhtNav ')
    		.click(function() {
-   			var currentImgSrc=$('.marginOneSevenPct.floatLeft img')[1].src;
+   			var currentImgSel=$('.floatLeft.indImgWrapper > img');
+   			var currentImgSrc=currentImgSel[1].src;
    			var nextIdx=NS.catgImgArray[currentImgSrc.substring(currentImgSrc.lastIndexOf('/')+1)]+1;
    			var nextImg,prefix;;
    			if (nextIdx < NS.catgNextImgArray.length) {
    	   			prefix=currentImgSrc.substr(0,currentImgSrc.lastIndexOf('/'));
 	   			nextImg=NS.catgNextImgArray[nextIdx];
-	   			$('.marginOneSevenPct > img')[0].src=currentImgSrc;
-	   			$('.marginOneSevenPct > img')[1].src=prefix+'/'+nextImg;
+	   			currentImgSel[0].src=currentImgSrc;
+	   			currentImgSel[1].src=prefix+'/'+nextImg;
    			}	
    		});
    $('.galleryLftNav ')
 		.click(function() {
-			var currentImgSrc=$('.marginOneSevenPct.floatLeft img')[0].src;
+   			var currentImgSel=$('.floatLeft.indImgWrapper > img');
+			var currentImgSrc=currentImgSel[0].src;
 			var nextIdx=NS.catgImgArray[currentImgSrc.substring(currentImgSrc.lastIndexOf('/')+1)]-1;
    			var nextImg,prefix;
 			if (nextIdx >= 0) {
 	   			prefix=currentImgSrc.substr(0,currentImgSrc.lastIndexOf('/'));
 	  			nextImg=NS.catgNextImgArray[nextIdx];
-	  			$('.marginOneSevenPct > img')[0].src=prefix+'/'+nextImg;
-	   			$('.marginOneSevenPct > img')[1].src=currentImgSrc;
+	  			currentImgSel[0].src=prefix+'/'+nextImg;
+	  			currentImgSel[1].src=currentImgSrc;
 			}	
 		});
  });
